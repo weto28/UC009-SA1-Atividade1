3adfff3a398310bc92116f34e7045235afe4f6a9
wellington Monteiro <wellington_log@hotmail.com.br>
2021-08-27 18:27:05 -0300

    Primeiro commit

---
+++ _javascript/direciona.js
@@ -0,0 +1,11 @@
+//Função para inserir o link no objeto #linkformulario
+function abrir(){
+    var mensagem="Clique aqui 1";
+    document.getElementById("linkformulario").innerHTML=mensagem;
+}
+
+//Função para fechar a janela
+function fechar(){
+    alert("Apenas clientes maiores de idade poderão participar das promoções");
+    window.close();
+}
